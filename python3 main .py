a33fda279b43ff715101f9b86627b19a1265d54d
alphakat <95404355+Awmdan1@users.noreply.github.com>
2024-10-20 13:01:41 +0700

    Delete python3 main .py

--- python3 main .py
+++
@@ -1,9 +0,0 @@
-if __name__ == "__main__":
-    faucet_url = "URL_FAUCET"
-    
-    # Membaca akun dari file JSON
-    with open("accounts.json", "r") as f:
-        accounts = json.load(f)
-
-    bot = FaucetBot(faucet_url, accounts)
-    bot.run()
